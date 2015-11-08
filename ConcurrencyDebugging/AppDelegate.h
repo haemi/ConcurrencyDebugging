d500ec51cdb1cbfaedf0617c17a8d584abdccfc3
Stefan Walkner <stefan@arkulpa.at>
2015-11-08 12:13:30 +0100

    Initial Commit

---
+++ ConcurrencyDebugging/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ConcurrencyDebugging
+//
+//  Created by Stefan Walkner on 08.11.15.
+//  Copyright © 2015 arkulpa GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ConcurrencyDebugging/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ConcurrencyDebugging
+//
+//  Created by Stefan Walkner on 08.11.15.
+//  Copyright © 2015 arkulpa GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
